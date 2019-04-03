38902d5c7025d811229c4932cb158d75e9b9d0c5
Rafael de Oliveira <rafaoliverdev@gmail.com>
2019-04-03 09:34:04 -0300

    init: editorconfig, babel-plugin-root-import, reactotron, devtools, etc.

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { StyleSheet, View } from 'react-native';
+
+import Todo from '~/components/Todo';
+
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Todo title="Fazer caféeee" />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+});
