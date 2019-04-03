3fd3cf70ac29bdb146935da496c406fbad4969ca
Rafael de Oliveira <rafaoliverdev@gmail.com>
2019-04-03 12:50:09 -0300

    update: comp Header now with Logout button

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,24 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { withNavigation } from 'react-navigation';
 
 import {
-  View, Text, TouchableOpacity, StatusBar,
+  View, Text, TouchableOpacity, StatusBar, AsyncStorage,
 } from 'react-native';
+
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 import styles from './styles';
 
-const Header = ({ title }) => (
-  <View style={styles.container}>
-    <StatusBar barStyle="dark-content" />
+class Header extends Component {
+  static propTypes = {
+    title: PropTypes.string.isRequired,
+    navigation: PropTypes.shape({
+      navigate: PropTypes.func,
+    }).isRequired,
+  };
 
-    <View style={styles.left} />
-    <Text style={styles.title}>{title}</Text>
-    <TouchableOpacity onPress={() => {}} />
-  </View>
-);
+  signOut = async () => {
+    const { navigation } = this.props;
 
-Header.propTypes = {
-  title: PropTypes.string.isRequired,
-};
+    await AsyncStorage.clear();
 
-export default Header;
+    navigation.navigate('Welcome');
+  };
+
+  render() {
+    const { title } = this.props;
+
+    return (
+      <View style={styles.container}>
+        <StatusBar barStyle="dark-content" />
+
+        <View style={styles.left} />
+        <Text style={styles.title}>{title}</Text>
+        <TouchableOpacity onPress={this.signOut}>
+          <Icon name="exchange" size={16} style={styles.icon} />
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
+
+export default withNavigation(Header);
