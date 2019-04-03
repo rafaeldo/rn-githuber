575d20e1afe0153cb49369348610c1fa3e21ea82
Rafael de Oliveira <rafaoliverdev@gmail.com>
2019-04-03 14:31:11 -0300

    add: OrganizationItem comp

--- src/pages/Organizations/index.js
+++ src/pages/Organizations/index.js
@@ -1,17 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-import { View } from 'react-native';
+import api from '~/services/api';
+
+import {
+  View,
+  Text,
+  FlatList,
+  AsyncStorage,
+  ActivityIndicator,
+} from 'react-native';
 
 import Icon from 'react-native-vector-icons/FontAwesome';
 
 import Header from '~/components/Header';
+import OrganizationItem from './OrganizationItem';
 
-const Organizations = () => (
-  <View>
-    <Header title="Organizações" />
-  </View>
-);
+import styles from './styles';
 
 //
 //
@@ -21,10 +26,64 @@ const TabIcon = ({ tintColor }) => <Icon name="building" size={20} color={tintCo
 TabIcon.propTypes = {
   tintColor: PropTypes.string.isRequired,
 };
-
-Organizations.navigationOptions = {
-  tabBarIcon: TabIcon,
-};
 //
 
-export default Organizations;
+export default class Organizations extends Component {
+  static navigationOptions = {
+    tabBarIcon: TabIcon,
+  };
+
+  state = {
+    data: [],
+    loading: true,
+    refreshing: false,
+  };
+
+  componentDidMount() {
+    this.loadOrganizations();
+  }
+
+  loadOrganizations = async () => {
+    this.setState({ refreshing: true });
+
+    const username = await AsyncStorage.getItem('@Githuber:username');
+
+    const { data } = await api.get(`/users/${username}/orgs`);
+
+    this.setState({ data, loading: false, refreshing: false });
+  }
+
+  renderList = () => {
+    const { data, refreshing } = this.state;
+
+    if (data.length === 0) {
+      return (<Text style={styles.noOrgs}>Sem organizações.</Text>);
+    }
+
+    return (
+      <FlatList
+        data={data}
+        keyExtractor={item => String(item.id)}
+        renderItem={this.renderListItem}
+        numColumns={2}
+        columnWrapperStyle={styles.columnWrapper}
+        onRefresh={this.loadOrganizations}
+        refreshing={refreshing}
+      />
+    );
+  }
+
+  renderListItem = ({ item }) => <OrganizationItem organization={item} />
+
+  render() {
+    const { loading } = this.state;
+
+    return (
+      <View style={styles.container}>
+        <Header title="Organizações" />
+
+        { loading ? <ActivityIndicator style={styles.loading} /> : this.renderList() }
+      </View>
+    );
+  }
+}
