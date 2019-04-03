8cba6870e63b5548962542d2791823c686707986
Rafael de Oliveira <rafaoliverdev@gmail.com>
2019-04-03 11:41:32 -0300

    add: new comp Header inside Repositories page.

--- src/pages/Repositories/index.js
+++ src/pages/Repositories/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 import { View, Text } from 'react-native';
 
+import Header from '~/components/Header';
+
 // import styles from './styles';
 
 const Repositories = () => (
   <View>
-    <Text>Repositórios</Text>
+    <Header title="Repositórios" />
   </View>
 );
 
